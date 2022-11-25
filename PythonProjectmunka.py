8cd87c8d58f86ac7615cfc7a12dc79f1960f9c4a
bayerbalazs <bayerbalazs01@gmail.com>
2022-11-25 19:20:11 +0100

    PP v0.0.3

--- PythonProjectmunka.py
+++ PythonProjectmunka.py
@@ -1,11 +1,29 @@
 import random
 Hetfo_DE =[]
+
 szek=0
 for szek in range(0,624,1) :
     i = random.randrange(0,2)
-    Hetfo_DE.append(i)
+    if i == 0 :
+        j = "F"
+    elif i == 1:
+        j = "U"
+    else:
+        print("valami error")        
+    Hetfo_DE.append(j)
     szek = szek + 1 
 
 print (Hetfo_DE)
-with open("Pproject.txt","r",encoding="utf-8") as kifile:
+with open("C:\git\PythonProject\Pproject.txt","w",encoding="utf-8") as kifile:
     kifile.writelines(Hetfo_DE)
+with open ("C:\git\PythonProject\Pproject.txt","r",encoding="utf-8") as befile:
+    for i in range:   
+        adat=sor.strip()
+        lista.append(adat)
+  
+print(foglalt)  
+
+
+
+
+
