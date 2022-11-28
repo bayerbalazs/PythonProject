4ead0062a19bb87e66a387254ab16955a4aebe7a
bayerbalazs <bayerbalazs01@gmail.com>
2022-11-28 12:19:15 +0100

    a foglaltság lekérdezése függvény

---
+++ lekerdezfilebol.py
@@ -0,0 +1,19 @@
+def lekerdez(nap,sor,szek):
+    foglalas=[]
+    hely=(int(sor) * 25) + int(szek)
+    print(hely)
+    print(True)
+    print(False)
+    with open("C:\git\PythonProject\Foglalas.txt","r",encoding="utf-8")as befile:
+        for sor in befile:
+            adat=sor.strip()
+            foglalas.append(adat)
+    print (foglalas[0])
+    print (foglalas[1])
+    print (foglalas[2])        
+        
+    if foglalas[nap][hely] == "U":
+        return False
+    else :
+        return True
+
