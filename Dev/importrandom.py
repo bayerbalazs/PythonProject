1da9856661c3a3c50f19ff64920eb7a46352df7b
bayerbalazs <bayerbalazs01@gmail.com>
2022-11-29 16:42:10 +0100

    új verzió

--- Dev/importrandom.py
+++ Dev/importrandom.py
@@ -4,10 +4,10 @@ foglrendszer=[]
 lista =[]
 szek= 0
 eloadas = 0
-for eloadas in range(0,3,1):
+for eloadas in range(0,21,1):
     
-    for szek in range(0,25,1):
-        i = random.randrange(0,2)
+    for szek in range(0,624,1):
+        i = 1
         if i == 0 :
             j = "F"
         elif i == 1:
@@ -25,7 +25,8 @@ print(eloadas,len(lista))
 a = len(lista)
 print(foglrendszer[0])
 print(foglrendszer[1])
-print(foglrendszer[2])
+print(foglrendszer[20])
 with open("C:\git\PythonProject\Foglalas.txt","w",encoding="utf-8") as kifile:
-    for out in range(0,3,1):
+    for out in range(0,21,1):
         kifile.writelines(foglrendszer[out])
+        print(out)
