a83df002ddf7c3f86f933725ac986c793596063e
bayerbalazs <bayerbalazs01@gmail.com>
2022-11-29 21:30:11 +0100

    -print

--- lekerdezfilebol.py
+++ lekerdezfilebol.py
@@ -2,14 +2,13 @@
 def lekerdez(nap,terem,eloadas,sor,szek):
     foglalas=[]
     nap1=(int(nap)-1)*9
-    terem1=int(terem)-1
+    terem1=(int(terem)-1)*3
     eloadas1=int(eloadas)-1
     sor1=int(sor)-1
     szek1=int(szek)-1
     hely=(sor1 * 25) + szek1
     ea=(nap1)+(eloadas1)+(terem1)
-    print(ea)
-    print(hely)
+    
     with open("C:\git\PythonProject\Foglalas.txt","r",encoding="utf-8")as befile:
         for sor in befile:
             adat=sor.strip()
