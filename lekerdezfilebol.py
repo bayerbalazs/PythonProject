0f78e9cdbee65f774b8b82cdf630d06c45e18254
bayerbalazs <bayerbalazs01@gmail.com>
2022-11-29 17:33:27 +0100

    4 input paraméterre átírva

--- lekerdezfilebol.py
+++ lekerdezfilebol.py
@@ -1,22 +1,21 @@
-def lekerdez(nap,sor,szek):
+#nap(1-7),eloadas(1-3),sor(1-25),szek(1-25)
+def lekerdez(nap,eloadas,sor,szek):
     foglalas=[]
-    hely=(int(sor) * 25) + int(szek)
-    eloadas=(int(nap))
+    hely=((int(sor)-1) * 25) + (int(szek)-1)
+    ea=(((int(nap)-1)*3)+(int(eloadas)-1))
     print(hely)
-    print(eloadas)
-    print(True)
-    print(False)
+    print(ea)
+    
     with open("C:\git\PythonProject\Foglalas.txt","r",encoding="utf-8")as befile:
         for sor in befile:
             adat=sor.strip()
             foglalas.append(adat)
-    print (foglalas[0])
-    print (foglalas[1])
-    print (foglalas[2])        
+            
         
-    if foglalas[eloadas][hely] == "U":
+    if   foglalas[ea][hely] == "U":
         return False
-    else :
+    elif foglalas[ea][hely] == "F":
         return True
-
-lekerdez (0,1,1)
+    else:
+        print("se F se U")
+print(lekerdez (1,3,1,1))
