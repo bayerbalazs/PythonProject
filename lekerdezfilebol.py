2b325d6c9921ce0580e7c20a9e14cbb6aea210b6
bayerbalazs <bayerbalazs01@gmail.com>
2022-11-29 18:54:48 +0100

    e

--- lekerdezfilebol.py
+++ lekerdezfilebol.py
@@ -1,15 +1,16 @@
-#nap(1-7),eloadas(1-3),sor(1-25),szek(1-25)
+#nap(1-7),terem(1-3)eloadas(1-3),sor(1-25),szek(1-25)
 def lekerdez(nap,terem,eloadas,sor,szek):
     foglalas=[]
     hely=((int(sor)-1) * 25) + (int(szek))
-    ea=(((int(nap)-1)*3)+(int(eloadas)-1))
+    ea=(((int(nap)-1)*3)+(int(eloadas)-1)+(int(terem)-1))
     print(hely)
     print(ea)
     with open("C:\git\PythonProject\Foglalas.txt","r",encoding="utf-8")as befile:
         for sor in befile:
             adat=sor.strip()
-            foglalas.append(adat)        
-    print(foglalas[0][624])    
+            foglalas.append(adat)  
+
+    print(foglalas[1][624])    
     if   foglalas[ea][hely] == "F":
         return True
     elif foglalas[ea][hely] == "U":
@@ -18,4 +19,4 @@ def lekerdez(nap,terem,eloadas,sor,szek):
         print("se F se U")
 
 
-print(lekerdez (1,1,25,25))
+print(lekerdez (1,1,1,25,25))
