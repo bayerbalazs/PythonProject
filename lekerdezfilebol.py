d00e7e2251a8e391752f0d4b7c6f4a00e2f8df4b
bayerbalazs <bayerbalazs01@gmail.com>
2022-11-29 18:23:19 +0100

    a 3 terem miatti változás

--- lekerdezfilebol.py
+++ lekerdezfilebol.py
@@ -1,21 +1,21 @@
 #nap(1-7),eloadas(1-3),sor(1-25),szek(1-25)
-def lekerdez(nap,eloadas,sor,szek):
+def lekerdez(nap,terem,eloadas,sor,szek):
     foglalas=[]
-    hely=((int(sor)-1) * 25) + (int(szek)-1)
+    hely=((int(sor)-1) * 25) + (int(szek))
     ea=(((int(nap)-1)*3)+(int(eloadas)-1))
     print(hely)
     print(ea)
-    
     with open("C:\git\PythonProject\Foglalas.txt","r",encoding="utf-8")as befile:
         for sor in befile:
             adat=sor.strip()
-            foglalas.append(adat)
-            
-        
-    if   foglalas[ea][hely] == "U":
-        return False
-    elif foglalas[ea][hely] == "F":
+            foglalas.append(adat)        
+    print(foglalas[0][624])    
+    if   foglalas[ea][hely] == "F":
         return True
+    elif foglalas[ea][hely] == "U":
+        return False
     else:
         print("se F se U")
-print(lekerdez (1,3,1,1))
+
+
+print(lekerdez (1,1,25,25))
