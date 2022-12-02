81a6bfb57986b0be1406da28302e0cd35b46743a
bayerbalazs <bayerbalazs01@gmail.com>
2022-12-02 18:36:42 +0100

    input adatok bekérése

--- Opera.py
+++ Opera.py
@@ -1,8 +1,13 @@
-#Egy operaház 3 termének a foglalási rendszere
+#Egy operaház 3 termének a foglalási rendszere egy hétre
 foglrendszer=[]
 lista =[]
 adat=[]
 kelltxt=input("Készüljön új foglalási file?(y/n)")
+napok=int(input("Kérlek add meg a foglalás napját.(1-7)"))
+termek=int(input("Kérlek add meg a foglalás termét.(1-3)"))
+eloadasok=int(input("Kérlek add meg a foglalás előadását.(1-3)"))
+sorok=int(input("Kérlek add meg a foglalás sorát.(1-25)"))
+szekek=int(input("Kérlek add meg a foglalás székét.(1-25)"))
 if kelltxt == "y":
     szek = 0
     eloadas = 0
@@ -19,7 +24,7 @@ else:
                 adat=sor.strip()
                 foglrendszer.append(adat)
 
-def lekerdez(nap,terem,eloadas,sor,szek):
+def lekerdezszeket(nap,terem,eloadas,sor,szek):
     foglalas=[]
     nap1=(int(nap)-1)*9
     terem1=(int(terem)-1)*3
@@ -37,6 +42,29 @@ def lekerdez(nap,terem,eloadas,sor,szek):
     if   foglalas[ea][hely] == "F":
         return True
     elif foglalas[ea][hely] == "U":
+        return False
+    else:
+        print("se F se U")
+
+def foglalszeket(nap,terem,eloadas,sor,szek):
+    foglalas=[]
+    nap1=(int(nap)-1)*9
+    terem1=(int(terem)-1)*3
+    eloadas1=int(eloadas)-1
+    sor1=int(sor)-1
+    szek1=int(szek)-1
+    hely=(sor1 * 25) + szek1
+    ea=(nap1)+(eloadas1)+(terem1)
+    
+    with open("C:\git\PythonProject\Foglalas.txt","r",encoding="utf-8")as befile:
+        for sor in befile:
+            adat=sor.strip()
+            foglalas.append(adat)  
+     
+    if   foglalas[ea][hely] == "U":
+        foglalas[ea][hely]="F"
+        return True
+    elif foglalas[ea][hely] == "F":        
         return False
     else:
         print("se F se U")
